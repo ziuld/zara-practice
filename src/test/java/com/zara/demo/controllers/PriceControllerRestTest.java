c4969ca8f11dcf286d2bb23eafc2e73e00bb67dd
Luis Daniel Hernandez <hernandezldm@gmail.com>
2023-04-17 12:57:28 +0200

    change PriceControllerRestTest

--- src/test/java/com/zara/demo/controllers/PriceControllerRestTest.java
+++ src/test/java/com/zara/demo/controllers/PriceControllerRestTest.java
@@ -10,9 +10,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -20,17 +24,22 @@ import java.util.Objects;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * TEST of management of prices
  * @author LD
  */
+@ExtendWith(MockitoExtension.class)
 public class PriceControllerRestTest {
+
     @Mock
     private PriceServiceImpl priceService;
 
     @InjectMocks
     private PriceControllerRest priceControllerRest;
+
 
     private final long brandId = 1;
     private final long productId = 35455;
@@ -41,7 +50,6 @@ public class PriceControllerRestTest {
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
         date = LocalDateTime.parse("2020-06-14T10:00");
         expectedPrice = new Price();
         brand=new Brand();
@@ -55,17 +63,22 @@ public class PriceControllerRestTest {
         expectedPrice.setStartDate(LocalDateTime.parse("2020-12-31T23:59"));
         expectedPrice.setPriority(0);
         expectedPrice.setCurrency("EUR");
+
     }
 
     @Test
-    public void getPriceOfProductTest() {
+    public void getPriceOfProductTest() throws Exception {
+        //when(priceService.getPriceOfProduct("2020-06-14T10:00", "35455", "1")).thenReturn(expectedPrice);
 
         when(priceService.getPriceOfProduct(Mockito.any(Brand.class), Mockito.eq(productId), Mockito.eq(date)))
                 .thenReturn(expectedPrice);
+
         ResponseEntity<Price> response = priceControllerRest.getPriceOfProduct(date, productId, brandId);
-        verify(priceService).getPriceOfProduct(Mockito.any(Brand.class), Mockito.eq(productId), Mockito.eq(date));
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(expectedPrice.getPrice(), Objects.requireNonNull(response.getBody()).getPrice());
+
+        verify(priceService).getPriceOfProduct(Mockito.any(Brand.class), Mockito.eq(productId), Mockito.eq(date));
+
 
 
     }
